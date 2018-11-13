Set maximum IDT time. Fixed Jacobian routine. Validation pending

src/LaunchMPI.py
@@ -49,6 +49,8 @@ if __name__ == '__main__':
 
     # Initializations and preliminaries
     Global.MPI = MPI
+    # Maximum ignition delay time
+    Global.tMax = 1.0
     comm = MPI.COMM_WORLD   # get MPI communicator object
     size = comm.size        # total number of processes
     rank = comm.rank        # rank of this process
@@ -209,7 +211,8 @@ if __name__ == '__main__':
             # TODO : Modify this for new problem
             tasks.append((case, x0, taskindex))
         print "Before launching: ", tasks
-        quantityrefs = np.array(Par.tasklaunch(tasks))
+        quantityrefs_new = np.array(Par.tasklaunch(tasks))
+        quantityrefs.append(quantityrefs_new)
         print "Done launching"
         print "Computing reference cases"
 

src/OptimFunctions.py
@@ -147,6 +147,8 @@ def gradient_constraints(xf):
 
     # Collecting results
     stack = np.empty((0,noptim),float)
+    print "Quantity refs: ", quantityrefs
+    print "Perturb quantities: ", perturbquantities
     for kcase in range(num_lwcon):
         grad = (np.abs(perturbquantities[kcase, :]-quantityrefs[kcase])
                 - np.abs(initquantities[kcase]-quantityrefs[kcase]))/step
@@ -176,9 +178,10 @@ def gradient_constraints(xf):
     perturbquantities = results[len(cases):].reshape((len(cases), noptim))
 
     for kcase, case in enumerate(cases):
+        print kcase, len(cases)
         grad = (np.abs(perturbquantities[kcase, :]-quantityrefs[kcase])
                 - np.abs(initquantities[kcase]-quantityrefs[kcase]))/step
-        stack = np.hstack((stack, grad))
+        stack = np.vstack((stack, grad))
 
     # Logging info
     logging.info('Gradient G done')
@@ -213,6 +216,6 @@ def gradient_constraints(xf):
         logging.info('Current vector')
         logging.info(str(x).replace('  ', ','))
 
-    logging.info('End Gradient PP')
+    logging.info('End Gradient constraints')
 
     return stack