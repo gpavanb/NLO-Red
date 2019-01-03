Very crucial bug. Derivative test passed. Optimal converges also

src/LaunchMPI.py
@@ -207,7 +207,8 @@ if __name__ == '__main__':
           
           # Create gradient locations
           # Evaluate gradient using perturbation
-          step = 1e-2
+          Global.step = 1e-2
+          step = Global.step
           grad_X = np.empty((0,ndim))
           for i in xrange(num_samples):
             for j in xrange(ndim):
@@ -421,9 +422,9 @@ if __name__ == '__main__':
 
             nlp.num_option('tol',1e-3)
             nlp.num_option('acceptable_tol',1e-3)
-            #nlp.num_option('derivative_test_perturbation',1e-5)
-            #nlp.str_option('derivative_test','first-order')
-            #nlp.num_option('derivative_test_tol',1e-2)
+            nlp.num_option('derivative_test_perturbation',Global.step)
+            nlp.str_option('derivative_test','first-order')
+            nlp.num_option('derivative_test_tol',1e-2)
 
             logging.info('Starting optimization')
             x, zl, zu, constraint_multipliers, obj, status = nlp.solve(x0)

src/OptimFunctions.py
@@ -27,7 +27,7 @@ def eval_grad_f(xf, user_data=None):
 
     # Evaluate gradient using perturbation
     perturbquantities = np.empty(noptim,float)
-    step = 1e-2
+    step = Global.step
     results = []
     for kx in range(noptim):
         xp = np.copy(xf)
@@ -123,7 +123,7 @@ def gradient_constraints(xf):
  
     # Compute perturbations
     perturbquantities = np.empty((num_lwcon,0),float)
-    step = 1e-5
+    step = Global.step
     results = []
     for kx in range(noptim):
         xperturb = np.copy(xf)
@@ -163,7 +163,7 @@ def gradient_constraints(xf):
         tasks.append((case, x_orig, taskindex))
 
     # Perturbed quantities
-    step = 1e-5
+    step = Global.step
     for kcase, case in enumerate(cases):
         for kx in range(noptim):
             xperturb = np.copy(xf)
@@ -182,9 +182,10 @@ def gradient_constraints(xf):
     if (len(cases) != 0):
         perturbquantities = np.vstack((perturbquantities,perturb_new))
 
-    for kcase, case in enumerate(cases):
-        grad = (np.abs(perturbquantities[kcase, :]-quantityrefs[kcase])
-                - np.abs(initquantities[kcase]-quantityrefs[kcase]))/step
+    for kcase in xrange(len(cases)):
+        idx = kcase + num_lwcon # Only nonlinear constraints computed here
+        grad = (np.abs(perturbquantities[idx, :]-quantityrefs[idx])
+                - np.abs(initquantities[idx]-quantityrefs[idx]))/step
         stack = np.vstack((stack, grad))
 
     # Logging info
@@ -195,7 +196,6 @@ def gradient_constraints(xf):
     logging.info(str(np.sum(x)))
     logging.info('Current distance vector')
     logging.info(str(np.abs(initquantities-quantityrefs)))
-    print "IQ:", initquantities
 
     candidatecheck = True
     errortab = np.abs(initquantities-quantityrefs)